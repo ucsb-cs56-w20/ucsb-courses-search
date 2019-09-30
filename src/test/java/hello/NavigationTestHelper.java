727b1d948c30e9a2850867071ea369b01357e627
Phill Conrad <pconrad@cs.ucsb.edu>
2019-09-29 21:48:45 -0700

    pc - add navigation header and footer plus tests

---
+++ src/test/java/hello/NavigationTestHelper.java
@@ -0,0 +1,24 @@
+package hello;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.xpath;
+
+public class NavigationTestHelper {
+
+    public static void hasNavBar(MockMvc mvc, String url) throws Exception  {
+        mvc.perform(MockMvcRequestBuilders.get(url).accept(MediaType.TEXT_HTML))
+        .andExpect(status().isOk())
+        .andExpect(xpath("//nav").exists());
+
+    }
+
+    public static void hasFooter(MockMvc mvc, String url) throws Exception  {
+        mvc.perform(MockMvcRequestBuilders.get(url).accept(MediaType.TEXT_HTML))
+        .andExpect(status().isOk())
+        .andExpect(xpath("//footer").exists());
+
+    }
+}

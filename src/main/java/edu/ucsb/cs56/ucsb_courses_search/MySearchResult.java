d43d9f95426dd77b253e5f72a3eda1a68152b4ba
Phill Conrad <pconrad@cs.ucsb.edu>
2019-10-21 18:14:56 -0700

    pc WIP work towards course selection

---
+++ src/main/java/edu/ucsb/cs56/ucsb_courses_search/MySearchResult.java
@@ -0,0 +1,36 @@
+package edu.ucsb.cs56.ucsb_courses_search;
+
+public class MySearchResult  {
+
+    public MySearchResult() {
+
+    }
+
+    private String subjectArea;
+    private String quarter;
+    private String courseLevel;
+
+    public String getSubjectArea() {
+        return this.subjectArea;
+    }
+    public void setSubjectArea(String subjectArea) {
+        this.subjectArea = subjectArea;
+    }
+
+    public String getQuarter() {
+        return this.quarter;
+    }
+    public void setQuarter(String quarter) {
+        this.quarter = quarter;
+    }
+
+    public String getCourseLevel() {
+        return this.courseLevel;
+    }
+    public void setCourseLevel(String courseLevel) {
+        this.courseLevel = courseLevel;
+    }
+
+    
+
+} 

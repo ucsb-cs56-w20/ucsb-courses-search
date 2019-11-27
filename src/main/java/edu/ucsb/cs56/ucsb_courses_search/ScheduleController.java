b2a37123820b0d3ae6d57a98e64847e1fbacd011
justintjoa <justintoja@gmail.com>
2019-11-26 17:56:28 -0800

    added table, will fill with database soon

--- src/main/java/edu/ucsb/cs56/ucsb_courses_search/ScheduleController.java
+++ src/main/java/edu/ucsb/cs56/ucsb_courses_search/ScheduleController.java
@@ -6,12 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class GreetingController {
+public class ScheduleController {
 
-    @GetMapping("/greeting")
+    @GetMapping("/courseschedule")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 	model.addAttribute("name", name);
-	return "greeting"; // corresponds to src/main/resources/templates/greeting.html
+	return "courseschedule/index"; // corresponds to src/main/resources/templates/greeting.html
     }
+
     
 }

2f23e2a17ada0412d44e64b202e078b7ee5f0801
justintjoa <justintoja@gmail.com>
2019-11-28 23:13:56 -0800

    added javabean for the course entries to schedule

---
+++ src/main/java/edu/classes/entities/course.java
@@ -0,0 +1,60 @@
+package classes.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class course {
+ 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String classname;
+    private String professor;
+    private String meettime;
+    private String meetday;
+    private String location;
+
+    public String getClassname() {
+        return classname;
+    }
+
+    public String getProfessor() {
+        return professor;
+    }
+
+    public String getMeettime() {
+        return meettime;
+    }
+
+    public String getMeetday() {
+        return meetday;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setClassname(String classname) { 
+        this.classname = classname; 
+    }
+
+    public void setProfessor(String professor) { 
+        this.professor = professor; 
+    }
+
+    public void setMeettime(String meettime) { 
+        this.meettime =  meettime; 
+    }
+
+    public void setMeetday(String meetday) { 
+        this.meetday = meetday; 
+    }
+
+    public void setLocation(String location) { 
+        this.location = location; 
+    }
+
+    
+}

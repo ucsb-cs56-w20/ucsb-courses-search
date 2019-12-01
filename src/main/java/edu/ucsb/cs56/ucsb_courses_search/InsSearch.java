6ea18f53af0b49fea619d5c9efd2d7a741329764
Michael Zhang <mzhang0808@gmail.com>
2019-11-30 20:29:19 -0800

    mz/jk added java beans for InstructorSearchController & simplicity

---
+++ src/main/java/edu/ucsb/cs56/ucsb_courses_search/InsSearch.java
@@ -0,0 +1,28 @@
+package edu.ucsb.cs56.ucsb_courses_search;
+
+public class InsSearch{
+	private String instructor;
+	private String quarter;
+	
+	public InsSearch(){
+		instructor = "";
+		quarter = "";
+	}
+
+	public void setInstructor(String s){
+		this.instructor = s;
+	}
+
+	public void setQuarter(String q){
+		this.quarter = q;
+	}
+
+	public String getInstructor(){
+		return this.instructor;
+	}
+
+	public String getQuarter(){
+		return this.quarter;
+	}
+}
+

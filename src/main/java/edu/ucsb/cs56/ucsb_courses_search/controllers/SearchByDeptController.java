f3d3e4ed6e975c9d763c90ea041f25ce5b1be512
Phill Conrad <pconrad@cs.ucsb.edu>
2019-12-07 16:19:22 -0800

    pc - add search by department as part of cleaning up and refactoring main search

---
+++ src/main/java/edu/ucsb/cs56/ucsb_courses_search/controllers/SearchByDeptController.java
@@ -0,0 +1,58 @@
+package edu.ucsb.cs56.ucsb_courses_search.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import edu.ucsb.cs56.ucsb_courses_search.CurriculumService;
+import edu.ucsb.cs56.ucsb_courses_search.searches.SearchByDept;
+import edu.ucsb.cs56.ucsbapi.academics.curriculums.v1.classes.Course;
+import edu.ucsb.cs56.ucsbapi.academics.curriculums.v1.classes.CoursePage;
+import edu.ucsb.cs56.ucsbapi.academics.curriculums.utilities.Quarter;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Controller
+public class SearchByDeptController {
+
+    private Logger logger = LoggerFactory.getLogger(SearchByDeptController.class);
+
+    @Autowired   
+    private CurriculumService curriculumService;
+
+    
+    @GetMapping("/search/bydept")
+    public String instructor(Model model, SearchByDept searchByDept) {
+    	model.addAttribute("searchByDept", new SearchByDept());
+        return "search/bydept/search"; 
+    }
+
+    @GetMapping("/search/bydept/results")
+    public String search(
+        @RequestParam(name = "dept", required = true) 
+        String dept,
+        @RequestParam(name = "quarter", required = true) 
+        String quarter,
+        @RequestParam(name = "courseLevel", required = true) 
+        String courseLevel,
+        Model model, SearchByDept searchByDept
+        ) {
+        model.addAttribute("dept", dept);
+        model.addAttribute("quarter", quarter);
+        model.addAttribute("courseLevel", courseLevel);
+
+        String json = curriculumService.getJSON(dept,quarter,courseLevel);
+        CoursePage cp = CoursePage.fromJSON(json);
+        
+        model.addAttribute("cp",cp);
+        return "search/bydept/results"; 
+    }
+
+}

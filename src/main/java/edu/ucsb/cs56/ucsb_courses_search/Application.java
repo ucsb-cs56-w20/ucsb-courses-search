4b798786d93d3fbbf337c4d0ec0fd036d918a08e
Phill Conrad <pconrad@cs.ucsb.edu>
2019-12-09 23:59:03 -0800

    pc - remove greeting from url paths

--- src/main/java/edu/ucsb/cs56/ucsb_courses_search/Application.java
+++ src/main/java/edu/ucsb/cs56/ucsb_courses_search/Application.java
@@ -18,7 +18,7 @@ public class Application extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/", "/greeting", "/login**", "/webjars/**", "/error**", "/searchResults**", "/search/**") 
+                .antMatchers("/", "/login**", "/webjars/**", "/error**", "/searchResults**", "/search/**") 
                    .permitAll()
                 .anyRequest().authenticated()
                 .and()

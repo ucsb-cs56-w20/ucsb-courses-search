b1c90cb8cfff05b4284bc0e5c2f54e2e37995bcb
Phill Conrad <pconrad@cs.ucsb.edu>
2019-12-09 21:12:04 -0800

    pc - update by course search

--- src/main/java/edu/ucsb/cs56/ucsb_courses_search/controllers/SearchByCourseController.java
+++ src/main/java/edu/ucsb/cs56/ucsb_courses_search/controllers/SearchByCourseController.java
@@ -7,14 +7,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import edu.ucsb.cs56.ucsb_courses_search.CurriculumService;
+import edu.ucsb.cs56.ucsb_courses_search.results.CourseListingRow;
+import edu.ucsb.cs56.ucsb_courses_search.results.CourseOffering;
 import edu.ucsb.cs56.ucsb_courses_search.searches.SearchByCourse;
 import edu.ucsb.cs56.ucsbapi.academics.curriculums.v1.classes.Course;
 import edu.ucsb.cs56.ucsbapi.academics.curriculums.v1.classes.CoursePage;
 import edu.ucsb.cs56.ucsbapi.academics.curriculums.utilities.Quarter;
 
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,40 +26,42 @@ public class SearchByCourseController {
 
     private Logger logger = LoggerFactory.getLogger(SearchByCourseController.class);
 
-    @Autowired   
+    @Autowired
     private CurriculumService curriculumService;
 
-    
     @GetMapping("/search/bycourse")
     public String instructor(Model model, SearchByCourse searchByCourse) {
-    	model.addAttribute("searchByCourse", new SearchByCourse());
-        return "search/bycourse/search"; 
+        model.addAttribute("searchByCourse", new SearchByCourse());
+        return "search/bycourse/search";
     }
 
     @GetMapping("/search/bycourse/results")
-    public String search(
-        @RequestParam(name = "course", required = true) 
-        String course,
-        @RequestParam(name = "beginQ", required = true) 
-        int beginQ,
-        @RequestParam(name = "endQ", required = true) 
-        int endQ,
-        Model model, SearchByCourse searchByCourse
-        ) {
+    public String search(@RequestParam(name = "course", required = true) String course,
+            @RequestParam(name = "beginQ", required = true) int beginQ,
+            @RequestParam(name = "endQ", required = true) int endQ, Model model, SearchByCourse searchByCourse) {
         model.addAttribute("course", course);
         model.addAttribute("beginQ", beginQ);
         model.addAttribute("endQ", endQ);
 
         List<Course> courses = new ArrayList<Course>();
-        for(Quarter qtr = new Quarter(beginQ); qtr.getValue() <= endQ; qtr.increment()) {
-            String json = curriculumService.getCourse(course, qtr.getValue());   
-            logger.info("qtr=" + qtr.getValue() + " json="+json);         
+        for (Quarter qtr = new Quarter(beginQ); qtr.getValue() <= endQ; qtr.increment()) {
+            String json = curriculumService.getCourse(course, qtr.getValue());
+            logger.info("qtr=" + qtr.getValue() + " json=" + json);
             CoursePage cp = CoursePage.fromJSON(json);
             courses.addAll(cp.classes);
         }
-        
-        model.addAttribute("courses",courses);
-        return "search/bycourse/results"; 
+
+        model.addAttribute("courses", courses);
+
+        List<CourseOffering> courseOfferings = CourseOffering.fromCourses(courses);
+        List<CourseListingRow> rows = CourseListingRow.fromCourseOfferings(courseOfferings);
+
+        List<CourseListingRow> primaryRows = rows.stream().filter(r -> r.getRowType().equals("PRIMARY"))
+                .collect(Collectors.toList());
+
+        model.addAttribute("rows", primaryRows);
+
+        return "search/bycourse/results";
     }
 
 }

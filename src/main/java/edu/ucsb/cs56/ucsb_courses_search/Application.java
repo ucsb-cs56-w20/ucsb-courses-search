d98abed1dcbf73b47babebc4c23c00794b56527b
Phill Conrad <pconrad@cs.ucsb.edu>
2019-12-09 18:24:20 -0800

    pc - search by course should not require login

--- src/main/java/edu/ucsb/cs56/ucsb_courses_search/Application.java
+++ src/main/java/edu/ucsb/cs56/ucsb_courses_search/Application.java
@@ -18,10 +18,16 @@ public class Application extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/", "/greeting", "/login**", "/webjars/**", "/error**", "/searchResults**", "/search**") .permitAll()
-                .anyRequest().authenticated().and().oauth2Login().loginPage("/login").and().logout()
-                .deleteCookies("remove").invalidateHttpSession(true).logoutUrl("/logout").logoutSuccessUrl("/")
-                .permitAll();
+                .antMatchers("/", "/greeting", "/login**", "/webjars/**", "/error**", "/searchResults**", "/search/**") 
+                   .permitAll()
+                .anyRequest().authenticated()
+                .and()
+                  .oauth2Login().loginPage("/login")
+                .and()
+                   .logout()
+                     .deleteCookies("remove").invalidateHttpSession(true)
+                     .logoutUrl("/logout").logoutSuccessUrl("/")
+                     .permitAll();
     }
 
 }

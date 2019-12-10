159046bc9f03994a5a17e26b5682340f0b9a9b56
Phill Conrad <pconrad@cs.ucsb.edu>
2019-12-10 01:00:53 -0800

    pc - improve ux of multiquarter instructor search

--- src/main/java/edu/ucsb/cs56/ucsb_courses_search/controllers/SearchByInstructorController.java
+++ src/main/java/edu/ucsb/cs56/ucsb_courses_search/controllers/SearchByInstructorController.java
@@ -1,5 +1,9 @@
 package edu.ucsb.cs56.ucsb_courses_search;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,103 +14,139 @@ import edu.ucsb.cs56.ucsb_courses_search.CurriculumService;
 import edu.ucsb.cs56.ucsb_courses_search.InsSearchSpecific;
 import edu.ucsb.cs56.ucsb_courses_search.MySearchResult;
 import edu.ucsb.cs56.ucsbapi.academics.curriculums.v1.classes.CoursePage;
+import edu.ucsb.cs56.ucsbapi.academics.curriculums.v1.classes.Course;
+
+import edu.ucsb.cs56.ucsb_courses_search.results.CourseListingRow;
+import edu.ucsb.cs56.ucsb_courses_search.results.CourseOffering;
+import edu.ucsb.cs56.ucsb_courses_search.searches.SearchByCourse;
+import edu.ucsb.cs56.ucsb_courses_search.searches.SearchByInstructorMultiQuarter;
+import edu.ucsb.cs56.ucsbapi.academics.curriculums.utilities.Quarter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller
 public class SearchByInstructorController {
 
-    @Autowired   
+    private Logger logger = LoggerFactory
+            .getLogger(SearchByInstructorController.class);
+
+    @Autowired
     private CurriculumService curriculumService;
 
     // Hard code value for quarters
-    private static final String[] quarters = {"20174", "20181", "20182", "20183", "20184", "20191", "20192", "20193", "20194", "20201"};
+    private static final String[] quarters = { "20174", "20181", "20182", "20183", "20184", "20191", "20192", "20193",
+            "20194", "20201" };
 
-    @GetMapping("/search/byinstructor") 
+    @GetMapping("/search/byinstructor")
     public String instructor(Model model) {
-    	model.addAttribute("searchObject", new MySearchResult());
-        return "search/byinstructor/search"; 
+        model
+                .addAttribute("searchObject", new MySearchResult());
+        return "search/byinstructor/search";
     }
 
-	@GetMapping("/search/byinstructor/results")  
-    public String singleQtrSearch( InsSearch insSearch, Model model) {
-        model.addAttribute("insSearch", insSearch);
-        
-        // calls curriculumService method to get JSON from UCSB API
-        String json = curriculumService.getJSON(insSearch.getInstructor(), insSearch.getQuarter());
-        
-        // maps json to a CoursePage object so values can be easily accessed
-        CoursePage cp = CoursePage.fromJSON(json);
-        
-        // adds the json and CoursePage object as attributes so they can be accessed in the html, e.g. ${json} or ${cp.classes}
-        model.addAttribute("json",json);
-        model.addAttribute("cp",cp);
-        
-        return "search/byinstructor/results";
-    } 
+    @GetMapping("/search/byinstructor/results")
+    public String singleQtrSearch(InsSearch insSearch, Model model) {
+        model
+                .addAttribute("insSearch", insSearch);
 
-    @GetMapping("/search/byinstructor/specific")  // /search/instructor/specific
+        // calls curriculumService method to get JSON from UCSB API
+        String json = curriculumService
+                .getJSON(insSearch
+                        .getInstructor(),
+                        insSearch
+                                .getQuarter());
+
+        // maps json to a CoursePage object so values can be easily accessed
+        CoursePage cp = CoursePage
+                .fromJSON(json);
+
+        // adds the json and CoursePage object as attributes so they can be accessed in
+        // the html, e.g. ${json} or ${cp.classes}
+        model
+                .addAttribute("json", json);
+        model
+                .addAttribute("cp", cp);
+
+        return "search/byinstructor/results";
+    }
+
+    @GetMapping("/search/byinstructor/specific") // /search/instructor/specific
     public String specifc(Model model) {
-        model.addAttribute("searchObject", new MySearchResult());
-        return "search/byinstructor/specific/search"; 
-    }  
+        model
+                .addAttribute("searchObject", new MySearchResult());
+        return "search/byinstructor/specific/search";
+    }
 
     @GetMapping("/search/byinstructor/specific/results")
-    public String singleQtrSearch(
-        InsSearchSpecific insSearchSpecic,
-        Model model
-        ) {
-        model.addAttribute("insSearchSpecic", insSearchSpecic);
-        
+    public String singleQtrSearch(InsSearchSpecific insSearchSpecific, Model model) {
+        model
+                .addAttribute("insSearchSpecific", insSearchSpecific);
+
         // calls curriculumService method to get JSON from UCSB API
-        String json = curriculumService.getJSON(insSearchSpecic.getInstructor(), insSearchSpecic.getQuarter());
-        
+        String json = curriculumService
+                .getJSON(insSearchSpecific
+                        .getInstructor(),
+                        insSearchSpecific
+                                .getQuarter());
+
         // maps json to a CoursePage object so values can be easily accessed
-        CoursePage cp = CoursePage.fromJSON(json);
-        
-        // adds the json and CoursePage object as attributes so they can be accessed in the html, e.g. ${json} or ${cp.classes}
-        model.addAttribute("json",json);
-        model.addAttribute("cp",cp);
-        
-        return "search/byinstructor/specific/results"; 
-    }  
+        CoursePage cp = CoursePage
+                .fromJSON(json);
+
+        // adds the json and CoursePage object as attributes so they can be accessed in
+        // the html, e.g. ${json} or ${cp.classes}
+        model
+                .addAttribute("json", json);
+        model
+                .addAttribute("cp", cp);
+
+        return "search/byinstructor/specific/results";
+    }
 
     @GetMapping("/search/byinstructor/multiquarter") // search/instructor/multiquarter
-    public String multi(Model model) {
-    	model.addAttribute("searchObject", new MySearchResult());
-        return "search/byinstructor/multiquarter/search"; 
+    public String multi(Model model, SearchByInstructorMultiQuarter searchObject) {
+        model
+                .addAttribute("searchObject", new SearchByInstructorMultiQuarter());
+        model
+                .addAttribute("quarters", Quarter
+                        .quarterList("W20", "F83"));
+        return "search/byinstructor/multiquarter/search";
     }
 
     @GetMapping("/search/byinstructor/multiquarter/results")
     public String search(
         @RequestParam(name = "instructor", required = true) 
         String instructor,
-        @RequestParam(name = "beginQuarter", required = true) 
-        int beginQuarter,
-        @RequestParam(name = "endQuarter", required = true) 
-        int endQuarter,
-        Model model
-        ) {
-        model.addAttribute("instructor", instructor);
-        model.addAttribute("beginQuarter", beginQuarter);
-        model.addAttribute("endQuarter", endQuarter);
-        //model.addAttribute("quarter", quarter);
-        
-        // calls curriculumService method to get JSON from UCSB API
-        // Note: the same curriculum service as the single quarter search above is used, so we will need to implement a function that takes in multiple quarters
-	    String json = curriculumService.getJSON(instructor, quarters[beginQuarter]);
-        
-        // maps json to a CoursePage object so values can be easily accessed
-        CoursePage cp = CoursePage.fromJSON(json);
+        @RequestParam(name = "beginQ", required = true) 
+        int beginQ,
+        @RequestParam(name = "endQ", required = true) 
+        int endQ,
+        Model model,
+        SearchByInstructorMultiQuarter searchObject) {
 
-        for(int i = beginQuarter + 1; i <= endQuarter; i++){
-            cp.classes.addAll(CoursePage.fromJSON(curriculumService.getJSON(instructor, quarters[i])).classes);
-        }
-        
-        // adds the json and CoursePage object as attributes so they can be accessed in the html, e.g. ${json} or ${cp.classes}
-        model.addAttribute("json",json);
-        model.addAttribute("cp",cp);
-        
+            logger.info("GET request for /search/byinstructor/multiquarter/results");
+            logger.info("beginQ=" + beginQ + " endQ=" + endQ);
 
-        return "search/byinstructor/multiquarter/results"; 
+            List<Course> courses = new ArrayList<Course>();
+            for (Quarter qtr = new Quarter(beginQ); qtr.getValue() <= endQ; qtr.increment()) {
+                String json = curriculumService.getJSON(instructor, qtr.getYYYYQ());
+                logger.info("qtr=" + qtr.getValue() + " json=" + json);
+                CoursePage cp = CoursePage.fromJSON(json);
+                courses.addAll(cp.classes);
+            }
+
+            model.addAttribute("courses", courses);
+
+            List<CourseOffering> courseOfferings = CourseOffering.fromCourses(courses);
+            List<CourseListingRow> rows = CourseListingRow.fromCourseOfferings(courseOfferings);
+
+            model.addAttribute("rows", rows);
+            model.addAttribute("searchObject", searchObject );
+
+            // Note: F83 seems to be the oldest data available in the API
+            model.addAttribute("quarters",Quarter.quarterList("W20","F83"));
+            return "search/byinstructor/multiquarter/results";
     }
 
 }

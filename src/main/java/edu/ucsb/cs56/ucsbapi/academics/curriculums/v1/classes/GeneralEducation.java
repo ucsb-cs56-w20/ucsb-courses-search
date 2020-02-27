e550af8b85a66884d244c21c2096fbecbe9cbbbc
kristin-7 <dingchuanhu@ucsb.edu>
2020-02-26 17:19:37 -0800

    kh - add GeneralEducation.java

---
+++ src/main/java/edu/ucsb/cs56/ucsbapi/academics/curriculums/v1/classes/GeneralEducation.java
@@ -0,0 +1,10 @@
+package edu.ucsb.cs56.ucsbapi.academics.curriculums.v1.classes;
+
+import lombok.Data;
+
+@Data
+public class GeneralEducation {
+    public String geCode;
+    public String geCollege;
+    public GeneralEducation() {}
+}

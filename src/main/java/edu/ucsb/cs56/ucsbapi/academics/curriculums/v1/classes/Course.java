e00996a27d163c8336902e0d91edd2d3524e70c0
Jairo Bedolla <jairobedolla@ucsb.edu>
2020-03-03 12:17:02 -0800

    jb - used api to place description into th e main course search html page until button is finished

--- src/main/java/edu/ucsb/cs56/ucsbapi/academics/curriculums/v1/classes/Course.java
+++ src/main/java/edu/ucsb/cs56/ucsbapi/academics/curriculums/v1/classes/Course.java
@@ -18,6 +18,7 @@ public class Course {
     private String quarter;
     private String courseId;
     private String title;
+    private String description;
     private List<Section> classSections;
 
     public Course() {
@@ -53,6 +54,14 @@ public class Course {
 
     public void setClassSections(List<Section> classSections) {
         this.classSections = classSections;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public void setDescription(){
+        this.description = description;
     }
 
    

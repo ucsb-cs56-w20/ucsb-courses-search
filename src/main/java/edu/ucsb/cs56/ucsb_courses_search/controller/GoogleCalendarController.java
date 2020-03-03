1af126ffe5730fe1f143c3171571e6ffe4eaf175
Spencer Zou <spencerzou@ucsb.edu>
2020-03-03 12:19:46 -0800

    sz - GoogleCalendarController made with instructions for template

---
+++ src/main/java/edu/ucsb/cs56/ucsb_courses_search/controller/GoogleCalendarController.java
@@ -0,0 +1,11 @@
+package edu.ucsb.cs56.ucsb_courses_search.controller;
+
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+
+@Controller
+public class GoogleCalendarController {
+
+    @GetMapping("/GoogleCalendar")
+    //<button type="submit" th:formaction="@{/searchResults}" class="btn btn-primary" id="js-course-search-submit">Find Courses</button> use this format to create button in templates/courseschedule/index.html
+    //function goes here
+}

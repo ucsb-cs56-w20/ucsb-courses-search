eccc7563715281d7d832d8a565bd26bf2d15c05a
Isaac Lam <Isaac@169-231-211-24.wireless.ucsb.edu>
2020-03-04 13:54:51 -0800

    il made files CalendarService,UCSBCurrentQuarterCalendar, and QuarterDeadlines in first attempts to get info from api

---
+++ src/main/java/edu/ucsb/cs56/ucsbapi/academics/curriculums/v1/classes/QuarterDeadlines.java
@@ -0,0 +1,30 @@
+package edu.ucsb.cs56.ucsbapi.academics.curriculums.v1.classes;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import edu.ucsb.cs56.ucsbapi.academics.curriculums.utilities.Quarter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class QuarterDeadlines{
+    
+    private quarter;
+    private firstDayOfClasses;
+    private lastDayOfClasses;
+    private lastDayOfFinals;
+    private firstDayOfQuarter;
+    private pass1Begin;
+    private pass2Begin;
+    private pass3Begin;
+    private feeDeadline;
+    private lastDayToAddUnderGrad;
+    private lastDayToAddGrad;
+    private lastDayThirdWeek;
+
+
+    
+
+
+}

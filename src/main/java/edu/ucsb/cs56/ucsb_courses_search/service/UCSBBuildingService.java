0a157cdaef50577ae93d8e276750bbf769ef35d3
Ethan Pletcher <epletcher@cs.ucsb.edu>
2020-03-04 19:18:24 -0800

    ep/sm/cm-w-7pm-d- Finished Basic Maps

--- src/main/java/edu/ucsb/cs56/ucsb_courses_search/service/UCSBBuildingService.java
+++ src/main/java/edu/ucsb/cs56/ucsb_courses_search/service/UCSBBuildingService.java
@@ -11,13 +11,9 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.util.ResourceUtils;
 
 public class UCSBBuildingService {
-
-    private static Logger logger = LoggerFactory.getLogger(UCSBBuildingService.class);
 
     private static UCSBBuildingService instance = null;
 
@@ -27,35 +23,24 @@ public class UCSBBuildingService {
 
         this.buildingMap = new HashMap<String,UCSBBuilding>();
 
-        logger.info("before buildingMap="+buildingMap);
-
         try {
             ObjectMapper mapper = new ObjectMapper();
 
             File file = ResourceUtils.getFile("classpath:ucsb_buildings.json");
             InputStream is = new FileInputStream(file);
 
-            // InputStream is = TypeReference.class.getResourceAsStream("classpath:ucsb_buildings.json");
-
-            logger.info("is="+is);
-
             UCSBBuilding[] buildings = mapper.readValue(is, UCSBBuilding[].class);
-
             for (int i = 0; i < buildings.length; i++) {
-                this.buildingMap.put(buildings[i].getName(), buildings[i]);
+                this.buildingMap.put(buildings[i].getCode(), buildings[i]);
             }
 
-            logger.info("after buildingMap="+buildingMap);
-
         } catch (IOException e) {
-            logger.info("exception="+e);
             e.printStackTrace();
         }
 
     }
 
     public static UCSBBuildingService getInstance() {
-        logger.info("instance="+instance);
         if (instance == null) {
             instance = new UCSBBuildingService();
         }
@@ -69,12 +54,8 @@ public class UCSBBuildingService {
     }
 
     public static String getLink(String buildingCode){
-        logger.info("buildingCode="+buildingCode);
         UCSBBuildingService service = UCSBBuildingService.getInstance();
-        logger.info("service="+service);
         UCSBBuilding building = service.buildingMap.get(buildingCode);
-        logger.info("building="+building);
-
         if (building==null) {
             return "";
         }
@@ -82,7 +63,6 @@ public class UCSBBuildingService {
         String lon = Double.toString(building.getLongitude());
         String result = "https://maps.openrouteservice.org/directions?n1=34.412592&n2=-119.845887&n3=16&a="
         + lat + "," + lon + ",null,null&b=0&c=0&k1=en-US&k2=km";
-        logger.info("result="+result);
         return result;
     }
 }

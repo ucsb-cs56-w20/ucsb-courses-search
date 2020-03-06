3ee35b2eae21c9fe1cab2434ca6f7196358520b8
Isaac Lam <Isaac@169-231-220-245.wireless.ucsb.edu>
2020-03-05 18:36:49 -0800

    il/bk added UCSBQuarterCalendarService.java which implements QuarterCalendar, using UCSBAcademicCurriculum as guide

---
+++ src/main/java/edu/ucsb/cs56/ucsb_courses_search/service/UCSBQuarterCalendarService.java
@@ -0,0 +1,63 @@
+package edu.ucsb.cs56.ucsb_courses_search.service;
+
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Service object that wraps the UCSB Academic Curriculum API
+ */
+@Service
+public class UCSBQuarterCalendarService implements CalendarService {
+
+    private Logger logger = LoggerFactory.getLogger(UCSBQuarterCalendarService.class);
+
+    private String apiKey;
+
+    public UCSBQuarterCalendarService(@Value("${ucsb.api.consumer_key}") String apiKey) {
+        this.apiKey = apiKey;
+        logger.info("apiKey=" + apiKey);
+    }
+
+    public String getJSON(){
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("ucsb-api-version", "1.0");
+        headers.set("ucsb-api-key", this.apiKey);
+
+        HttpEntity<String> entity = new HttpEntity<>("body", headers);
+
+        String uri = "https://api.ucsb.edu/academics//quartercalendar/v1/quarters/current";
+        String url = uri;
+
+        logger.info("url=" + url);
+
+        String retVal = "";
+        try {
+            ResponseEntity<String> re = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+            MediaType contentType = re.getHeaders().getContentType();
+            HttpStatus statusCode = re.getStatusCode();
+            retVal = re.getBody();
+        } catch (HttpClientErrorException e) {
+            retVal = "{\"error\": \"401: Unauthorized\"}";
+        }
+        logger.info("from UCSBQuarterCalendarService.getJSON: " + retVal);
+        return retVal;
+    }
+}
+
+    

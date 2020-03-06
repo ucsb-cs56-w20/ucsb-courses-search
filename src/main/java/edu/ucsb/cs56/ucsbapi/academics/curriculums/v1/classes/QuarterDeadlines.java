a825b81e2df1172743a8d87b0d3721a4a6ead55a
Isaac Lam <Isaac@169-231-220-245.wireless.ucsb.edu>
2020-03-05 18:01:36 -0800

    il changed Quarterdeadlines so that it could compile

--- src/main/java/edu/ucsb/cs56/ucsbapi/academics/curriculums/v1/classes/QuarterDeadlines.java
+++ src/main/java/edu/ucsb/cs56/ucsbapi/academics/curriculums/v1/classes/QuarterDeadlines.java
@@ -9,22 +9,4 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class QuarterDeadlines{
-    
-    private quarter;
-    private firstDayOfClasses;
-    private lastDayOfClasses;
-    private lastDayOfFinals;
-    private firstDayOfQuarter;
-    private pass1Begin;
-    private pass2Begin;
-    private pass3Begin;
-    private feeDeadline;
-    private lastDayToAddUnderGrad;
-    private lastDayToAddGrad;
-    private lastDayThirdWeek;
-
-
-    
-
-
 }

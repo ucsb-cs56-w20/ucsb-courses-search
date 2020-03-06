1e13e34668f993696a23ff48352cbefce5ad6399
Isaac Lam <Isaac@169-231-220-245.wireless.ucsb.edu>
2020-03-05 18:39:25 -0800

    il/bk - added getters and setters and logger to quarterdeadlines.java

---
+++ src/main/java/edu/ucsb/cs56/ucsbapi/academics/curriculums/v1/classes/QuarterDeadlines.java
@@ -0,0 +1,28 @@
+package edu.ucsb.cs56.ucsbapi.academics.curriculums.v1.classes;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import edu.ucsb.cs56.ucsbapi.academics.curriculums.utilities.Quarter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class QuarterDeadlines{
+    
+    private Logger logger = LoggerFactory.getLogger(Course.class);
+
+    private String quarter;
+    private String firstDayOfClasses;
+    private String lastDayOfClasses;
+    private String lastDayOfFinals;
+    private String firstDayOfQuarter;
+    private String pass1Begin;
+    private String pass2Begin;
+    private String pass3Begin;
+    private String feeDeadline;
+    private String lastDayToAddUnderGrad;
+    private String lastDayToAddGrad;
+    private String lastDayThirdWeek;
+
+}

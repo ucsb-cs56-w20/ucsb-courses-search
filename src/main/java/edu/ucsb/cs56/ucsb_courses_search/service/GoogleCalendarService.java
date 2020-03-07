0401ea55e2506935af74e484f0b9c80922317112
unknown <dariansung@umail.ucsb.edu>
2020-03-07 13:36:25 -0800

    ds - Loaded client id and secret in GoogleCalendarService

--- src/main/java/edu/ucsb/cs56/ucsb_courses_search/service/GoogleCalendarService.java
+++ src/main/java/edu/ucsb/cs56/ucsb_courses_search/service/GoogleCalendarService.java
@@ -1,14 +1,5 @@
 package edu.ucsb.cs56.ucsb_courses_search.service;
 
-<<<<<<< HEAD
-=======
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-
-import java.io.FileNotFoundException;
->>>>>>> ds - moved /GoogleCalendar mapping to CourseController, deleted GoogleCalendarController
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.Collections;
@@ -26,28 +17,14 @@ import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.CalendarScopes;
 
-<<<<<<< HEAD
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import edu.ucsb.cs56.ucsbapi.academics.curriculums.v1.classes.Course;
-=======
->>>>>>> ds - moved /GoogleCalendar mapping to CourseController, deleted GoogleCalendarController
 @Service
 public class GoogleCalendarService {
-    
-    private static final String APPLICATION_NAME = "UCSB Courses Search Google Calendar Export";
-    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-    //private static final String CREDENTIALS_FILE_PATH = "../../localhost.json";
-    private static final List<String> SCOPES = Collections.singletonList(CalendarScopes.CALENDAR);
-    private static final String TOKENS_DIRECTORY_PATH = "tokens";
-    @Value("#{environment['spring.security.oauth2.client.registration.google.client-id']}")
-    private String clientId;
-    @Value("#{environment['spring.security.oauth2.client.registration.google.client-secret']}")
-    private String clientSecret;
-
     private static final String APPLICATION_NAME = "UCSB Courses Search Google Calendar Export";
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
     // private static final String CREDENTIALS_FILE_PATH = "../../localhost.json";
@@ -61,7 +38,6 @@ public class GoogleCalendarService {
 
     private Logger logger = LoggerFactory.getLogger(GoogleCalendarService.class);
 
-<<<<<<< HEAD
     // Must be called before createGoogleCalendar()
     public void setClasses(Iterable<Course> myclasses) {
         this.myclasses = myclasses;
@@ -69,29 +45,6 @@ public class GoogleCalendarService {
 
     public static Credential createCredentialWithAccessTokenOnly(TokenResponse tokenResponse) {
         return new Credential(BearerToken.authorizationHeaderAccessMethod()).setFromTokenResponse(tokenResponse);
-=======
-    private Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
-        // Load client secrets.
-        /*
-        InputStream in = GoogleCalendarService.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
-        if (in == null) {
-            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
-        }
-        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-        */
-
-        logger.info("Client ID: " + clientId);
-        logger.info("Client Secret: " + clientSecret);
-
-        // Build flow and trigger user authorization request.
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                HTTP_TRANSPORT, JSON_FACTORY, clientId, clientSecret, SCOPES)
-                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
-                .setAccessType("offline")
-                .build();
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8080).build();
-        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
->>>>>>> ds - moved /GoogleCalendar mapping to CourseController, deleted GoogleCalendarController
     }
 
     public Credential createCredentialWithRefreshToken(HttpTransport transport, JsonFactory jsonFactory, TokenResponse tokenResponse) {
@@ -108,15 +61,9 @@ public class GoogleCalendarService {
     // this function
     public void createGoogleCalendar() throws IOException, GeneralSecurityException {
         final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-<<<<<<< HEAD
         Credential credential = createCredentialWithRefreshToken(HTTP_TRANSPORT, JSON_FACTORY, new TokenResponse());
         Calendar service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
                 .setApplicationName(APPLICATION_NAME).build();
-=======
-        Calendar service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-        .setApplicationName(APPLICATION_NAME)
-        .build();
->>>>>>> ds - moved /GoogleCalendar mapping to CourseController, deleted GoogleCalendarController
         logger.info("Calendar built");
     }
 }

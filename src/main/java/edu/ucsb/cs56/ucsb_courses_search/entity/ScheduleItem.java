476ee19cc116b4897d38c11de72b0259607ccde5
Cathy Huang <cathyhuang@169-231-133-241.wireless.ucsb.edu>
2020-03-06 16:35:09 -0800

    ch/al/jj/jz/ks - renamed course to ScheduleItem

--- src/main/java/edu/ucsb/cs56/ucsb_courses_search/entity/ScheduleItem.java
+++ src/main/java/edu/ucsb/cs56/ucsb_courses_search/entity/ScheduleItem.java
@@ -6,7 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Course {
+public class ScheduleItem {
  
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)

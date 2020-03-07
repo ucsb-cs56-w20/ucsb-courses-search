41e75af1e05ed7d8d6ec5121dedf1a0ca67ab295
unknown <dariansung@umail.ucsb.edu>
2020-03-06 17:22:54 -0800

    ds - added stub for GoogleCalendarService

---
+++ src/main/java/edu/ucsb/cs56/ucsb_courses_search/service/GoogleCalendarService.java
@@ -0,0 +1,10 @@
+package edu.ucsb.cs56.ucsb_courses_search.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GoogleCalendarService {
+
+    private Logger logger = LoggerFactory.getLogger(GoogleCalendarService.class);
+
+} 

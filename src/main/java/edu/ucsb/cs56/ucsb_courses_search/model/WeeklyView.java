e5821465d454b791c6c8dfc20ab3e417a62c6f06
Lan-Chi Ho <aileemint17@169-231-166-216.wireless.ucsb.edu>
2020-03-07 13:36:54 -0800

    lh-fixed both lecture start time and section start time to return the correct row number instead of a string and tested both

--- src/main/java/edu/ucsb/cs56/ucsb_courses_search/model/WeeklyView.java
+++ src/main/java/edu/ucsb/cs56/ucsb_courses_search/model/WeeklyView.java
@@ -74,74 +74,61 @@ public class WeeklyView {
       return 0;
     }
 
-    public String returnLectureStartTime(Course course){
-      String time = course.getAssociatedLectureTime();
-      String startHour = time.substring(0,5); //String will return 9:00 for 9:00 AM but 13:00 for 1 PM
-      return startHour;
+    public int returnLectureStartTime(Course course){
+      //TESTED
+      //RETURNS ThE ROW, where 8:00AM = 1, 8:30AM = 2
+
+      String times = course.getAssociatedLectureTime();
+      //String times = "11:00 - 11:50";
+      int hour= Integer.parseInt(times.substring(0,2));
+      int minutes = Integer.parseInt(times.substring(3,5));
+      int time = hour*60 + minutes;//time in minutes
+      int row = (time-480)/30+1;
+      return row;
     }
 
-    public String returnSectionStartTime(Course course){
-      String time = course.getMeettime();
-      String startHour = time.substring(0,5); //String will return 9:00 for 9:00 AM but 13:00 for 1 PM
-      return startHour;
+    public int returnSectionStartTime(Course course){
+      //TESTED
+      //RETURNS ThE ROW, where 8:00AM = 1, 8:30AM = 2
+
+      String times = course.getMeettime();
+      //String times = "11:00 - 11:50";
+      int hour= Integer.parseInt(times.substring(0,2));
+      int minutes = Integer.parseInt(times.substring(3,5));
+      int time = hour*60 + minutes;//time in minutes
+      int row = (time-480)/30+1;
+      return row;
     }
 
     public int returnClasslength(Course course, String LectureOrSection){
+      //TESTED
+      //Course course, String LectureOrSection
       //for rowspan nte that meetime gives the section time not lecture time in form 09:00-09:50
       // for a 75 minute class, usually 12:30-1:45 or 2-3:14, the hour is difference of 1
       //for a 50 min class, the hour is the same etc; 7-7:50
       //for a long labs, there will be first a 2 hour difference such as 14-16:30 or 14-16:50, then also check the minutes for an extra
-      String times = "";
-      if(LectureOrSection=="lecture"){
-          times = course.getAssociatedLectureTime();
-      }
-      else{
-          times = course.getMeettime();
-      }
+      String times = "12:30 - 13:45";
+      // if(LectureOrSection=="lecture"){
+      //     times = course.getAssociatedLectureTime();
+      // }
+      // else{
+      //     times = course.getMeettime();
+      // }
+
       //Put both times in minutes
+
       int hour1 = Integer.parseInt(times.substring(0,2));
       int hour2 = Integer.parseInt(times.substring(8,10));
-      //int hourDifference = hour2 - hour1;
+
       int minutes1 = Integer.parseInt(times.substring(3,5));
       int minutes2 = Integer.parseInt(times.substring(11,13));
-      //int minuteDifference = minutes2 - minutes1;
+
       int time1 = hour1*60 + minutes1;
       int time2 = hour2*60 + minutes2;
       int differenceInMinutes = time2 - time1; //note that 11:00-11:50= 50 amd 17:30-21:20 = 3 hr 50 min = 230 min
       differenceInMinutes+=15; //everytime ends either 15,50 or 20 so add extra
       return differenceInMinutes/30;//rowspan
 
-      // if(hourDifference == 0){
-      //   return 2;
-      // }
-      // else if (hourDifference == 1 || hourDifference == -11){//12:30-1:45
-      //   return 3;
-      // }
-      // else if (hourDifference == 2){ //11:00-13:30 or 16-18:50 = 3 hours
-      //   if(minuteDifference == 50){
-      //     return 6;
-      //   }
-      //   else if(minuteDifference == 30){
-      //     return 5;
-      //   }
-      // }
-      // else if (hourDifference == 3) {//could be 17:30-20:00 for 2.5 hours which is rowspan 5{
-      //   if (minuteDifference == -30){
-      //     return 5;
-      //   }
-      //   else if (minuteDifference == 50 ){//8:00-11:50 -4 hours which is rowspan { or 13:00-16:50
-      //     return 8;
-      //   }
-      //
-      //
-      // }
-      // else if (hourDifference ==4){//17:30-21:20 for chem1bl which is rowpan 8 for 4 hours
-      //   if(minuteDifference == -10){
-      //     return 8;
-      //   }
-      //
-      // }
-      //return 0;
     }
 
     public String returnSpanLecture(Course course){
@@ -166,7 +153,7 @@ public class WeeklyView {
       //return "stub";
     }
 
-    public String returnStub(Course course){
-      return course.getClassname();
+    public String returnStub(){
+      return "stub";
     }
 }

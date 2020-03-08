2cac81ce88776811e347ebf638aa5d89fa5c47b5
Lina Zeng <alinazengx@gmail.com>
2020-03-08 14:49:49 -0700

    added a non phelp test

--- src/test/java/edu/ucsb/cs56/ucsb_courses_search/controller/SearchByClassroomControllerTest.java
+++ src/test/java/edu/ucsb/cs56/ucsb_courses_search/controller/SearchByClassroomControllerTest.java
@@ -90,10 +90,10 @@ public class SearchByClassroomControllerTest {
     }
 
     @Test
-    public void test_get_F18_IV_THEA1() throws Exception {
+    public void test_get_M19_IV_THEA1() throws Exception {
         String url = "http://localhost:8080/search/byclassroom/results?quarter=20202&building=PHELP&classroom=3526";
         HtmlPage htmlPage = webClient.getPage(url);
-        String textToFind = "ANTH   160  ";
+        String textToFind = "INT      95A ";
         String xpath=String.format("//*[text()='%s']",textToFind);
         List<DomElement> elems = htmlPage.getByXPath(xpath);
         assertEquals(textToFind,elems.get(0).getTextContent());

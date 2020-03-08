f6f9691cc294903de540dced3c129c044a4589e7
dariansung <dariansung@umail.ucsb.edu>
2020-03-07 20:11:21 -0800

    ds - working on generating xs:datetime strings, saving for rebase

--- src/main/java/edu/ucsb/cs56/ucsb_courses_search/service/GoogleCalendarService.java
+++ src/main/java/edu/ucsb/cs56/ucsb_courses_search/service/GoogleCalendarService.java
@@ -2,6 +2,7 @@ package edu.ucsb.cs56.ucsb_courses_search.service;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -16,6 +17,10 @@ import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.CalendarScopes;
+import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.EventAttendee;
+import com.google.api.services.calendar.model.EventDateTime;
+import com.google.api.services.calendar.model.Events;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -35,35 +40,80 @@ public class GoogleCalendarService {
     @Value("${spring.security.oauth2.client.registration.google.client-secret}")
     private String clientSecret;
     private Iterable<Course> myclasses;
+    private String email;
 
     private Logger logger = LoggerFactory.getLogger(GoogleCalendarService.class);
 
     // Must be called before createGoogleCalendar()
-    public void setClasses(Iterable<Course> myclasses) {
+    public void initialize(Iterable<Course> myclasses, String email) {
         this.myclasses = myclasses;
+        this.email = email;
     }
 
     public static Credential createCredentialWithAccessTokenOnly(TokenResponse tokenResponse) {
         return new Credential(BearerToken.authorizationHeaderAccessMethod()).setFromTokenResponse(tokenResponse);
     }
 
-    public Credential createCredentialWithRefreshToken(HttpTransport transport, JsonFactory jsonFactory, TokenResponse tokenResponse) {
-     return new Credential.Builder(BearerToken.authorizationHeaderAccessMethod()).setTransport(
-        transport)
-        .setJsonFactory(jsonFactory)
-        .setTokenServerUrl(null)
-        .setClientAuthentication(new BasicAuthentication(clientId, clientSecret))
-        .build()
-        .setFromTokenResponse(tokenResponse);
-  }
+    public Credential createCredentialWithRefreshToken(HttpTransport transport, JsonFactory jsonFactory,
+            TokenResponse tokenResponse) {
+        logger.info("Client ID: " + clientId);
+        logger.info("Client Secret: " + clientSecret);
+        return new Credential.Builder(BearerToken.authorizationHeaderAccessMethod()).setTransport(transport)
+                .setJsonFactory(jsonFactory).setTokenServerUrl(null)
+                .setClientAuthentication(new BasicAuthentication(clientId, clientSecret)).build()
+                .setFromTokenResponse(tokenResponse);
+    }
 
     // Precondition: Must call setClasses(Iterable<Course> myclasses) before calling
     // this function
     public void createGoogleCalendar() throws IOException, GeneralSecurityException {
         final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-        Credential credential = createCredentialWithRefreshToken(HTTP_TRANSPORT, JSON_FACTORY, new TokenResponse());
+        Credential credential = createCredentialWithRefreshToken(HTTP_TRANSPORT, JSON_FACTORY, new TokenResponse().setScope("CalendarScopes.CALENDAR"));
         Calendar service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
                 .setApplicationName(APPLICATION_NAME).build();
         logger.info("Calendar built");
+
+        ArrayList<EventAttendee> attendees = new ArrayList<EventAttendee>();
+        attendees.add(new EventAttendee().setEmail(email));
+
+        for(Course c: myclasses){
+            Event event = new Event();
+            event.setSummary(c.getClassname());
+            event.setLocation(c.getLocation());
+            event.setAttendees(attendees);
+
+            //Test
+            getXsDT(c);
+        }
+    }
+
+    private EventDateTime getStart(Course c){
+        String xsDT = getXsDT(c);
+        return new EventDateTime();
+    }
+
+    private String getXsDT(Course c){
+        String xsDT = "";
+        logger.info("Assoc Lec Time: " + c.getAssociatedLectureTime());
+        if(c.getQuarter().contains("S20")){
+            if(c.getAssociatedLectureDay().contains("M")){
+                xsDT += "2020-03-30";
+            }
+            else if(c.getAssociatedLectureDay().contains("T")){
+                xsDT += "2020-03-31";
+            }
+            else if(c.getAssociatedLectureDay().contains("W")){
+                xsDT += "2020-04-01";
+            }
+            else if(c.getAssociatedLectureDay().contains("R")){
+                xsDT += "2020-04-02";
+            }
+            else{
+                xsDT += "2020-04-03";
+            }
+        }
+        xsDT += "T";
+        xsDT += c.getAssociatedLectureTime();
+        return xsDT;
     }
 }

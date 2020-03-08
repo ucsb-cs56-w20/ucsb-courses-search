c3b23bb0fb9c20eb1ecb7cbada3292f62a79ffc5
Ethan Pletcher <epletcher@cs.ucsb.edu>
2020-03-08 16:14:05 -0700

    ep/sm -WIP

--- src/main/java/edu/ucsb/cs56/ucsb_courses_search/controller/CourseController.java
+++ src/main/java/edu/ucsb/cs56/ucsb_courses_search/controller/CourseController.java
@@ -7,13 +7,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import edu.ucsb.cs56.ucsb_courses_search.entity.Course;
+import edu.ucsb.cs56.ucsb_courses_search.model.Route;
 import edu.ucsb.cs56.ucsb_courses_search.repository.CourseRepository;
 import edu.ucsb.cs56.ucsb_courses_search.service.MembershipService;
+import edu.ucsb.cs56.ucsb_courses_search.service.UCSBBuildingService;
+
 import org.springframework.beans.factory.annotation.Qualifier;
 
 import java.util.ArrayList;
@@ -64,6 +68,36 @@ public class CourseController {
         }
         return "courseschedule/index";
     }
+
+    @GetMapping("courseschedule/walktime")
+    public String walkRouteForm(Model model) {
+      model.addAttribute("Route", new Route());
+      return "Route";
+    }
+  
+    @PostMapping("courseschedule/walktime")
+    public String walkRouteSubmit(@ModelAttribute Route route) {
+        String l1 = (route.getB1()).split("-")[0];
+        String l2 = (route.getB2()).split("-")[0];
+        String link = UCSBBuildingService.getWalkingLink(l1, l2);
+      return "redirect : " + link;
+    }
+
+    @GetMapping("courseschedule/biketime")
+    public String bikeRouteForm(Model model) {
+      model.addAttribute("Route", new Route());
+      return "Route";
+    }
+  
+    @PostMapping("courseschedule/biketime")
+    public String bikeRouteSubmit(@ModelAttribute Route route) {
+        String l1 = (route.getB1()).split("-")[0];
+        String l2 = (route.getB2()).split("-")[0];
+        String link = UCSBBuildingService.getBikingLink(l1, l2);
+      return "redirect : " + link;
+    }
+  
+  
     @PostMapping("/courseschedule/add")
     public String add(Course course, 
                         Model model,

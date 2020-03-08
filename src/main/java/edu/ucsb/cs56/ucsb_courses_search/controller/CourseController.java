e5dc74c92cb1703f1b263cd390f7c0f7e6985743
Lan-Chi Ho <aileemint17@169-231-166-216.wireless.ucsb.edu>
2020-03-07 22:02:10 -0800

    lh-trying to fix rebase

--- src/main/java/edu/ucsb/cs56/ucsb_courses_search/controller/CourseController.java
+++ src/main/java/edu/ucsb/cs56/ucsb_courses_search/controller/CourseController.java
@@ -59,6 +59,7 @@ public class CourseController {
             String[] days = new String[]{"Monday","Tuesday","Wednesday","Thursday","Friday"};
             String[] timerange = new String[]{"8:00 AM", "8:30 AM", "9:00 AM", "9:30 AM", "10:00 AM", "10:30 AM", "11:00 AM", "11:30 AM", "12:00 PM", "12:30 PM", "1:00 PM", "1:30 PM", "2:00 PM", "2:30 PM", "3:00 PM", "3:30 PM", "4:00 PM", "4:30 PM", "5:00 PM", "5:30 PM"};
             WeeklyView week = new WeeklyView();
+
             model.addAttribute("myclasses", myclasses);
             model.addAttribute("days", days);
             model.addAttribute("timerange", timerange);

82d8377a937ea2c0aa5d61b00ee943c603a65fdd
zeyuhe <zeyuhe@ucsb.edu>
2020-03-08 14:08:25 -0700

    hh - added the create a specific fake event method, cannot create event according to user's schedual yet

--- src/main/java/edu/ucsb/cs56/ucsb_courses_search/service/GoogleCalendarService.java
+++ src/main/java/edu/ucsb/cs56/ucsb_courses_search/service/GoogleCalendarService.java
@@ -197,4 +197,46 @@ public class GoogleCalendarService {
         recStr += "T170000Z";
         return recStr;
     }
+
+    private CreateEvent(){
+        Event event = new Event()
+            .setSummary("Google I/O 2015")
+            .setLocation("800 Howard St., San Francisco, CA 94103")
+            .setDescription("A chance to hear more about Google's developer products.");
+
+        DateTime startDateTime = new DateTime("2020-05-28T09:00:00-07:00");
+        EventDateTime start = new EventDateTime()
+            .setDateTime(startDateTime)
+            .setTimeZone("America/Los_Angeles");
+        event.setStart(start);
+
+        DateTime endDateTime = new DateTime("2015-05-28T17:00:00-07:00");
+        EventDateTime end = new EventDateTime()
+            .setDateTime(endDateTime)
+            .setTimeZone("America/Los_Angeles");
+        event.setEnd(end);
+
+        String[] recurrence = new String[] {"RRULE:FREQ=DAILY;COUNT=2"};
+        event.setRecurrence(Arrays.asList(recurrence));
+
+        EventAttendee[] attendees = new EventAttendee[] {
+            new EventAttendee().setEmail("lpage@example.com"),
+            new EventAttendee().setEmail("sbrin@example.com"),
+        };
+        event.setAttendees(Arrays.asList(attendees));
+
+        EventReminder[] reminderOverrides = new EventReminder[] {
+            new EventReminder().setMethod("email").setMinutes(24 * 60),
+            new EventReminder().setMethod("popup").setMinutes(10),
+        };
+        Event.Reminders reminders = new Event.Reminders()
+            .setUseDefault(false)
+            .setOverrides(Arrays.asList(reminderOverrides));
+        event.setReminders(reminders);
+
+        String calendarId = "primary";
+        event = service.events().insert(calendarId, event).execute();
+        System.out.printf("Event created: %s\n", event.getHtmlLink());
+    }
+
 }

a106048ecdbae574e5fd5a0502ca296469891c52
Ramon Verdin <ramonverdin@umail.ucsb.edu>
2020-03-08 17:59:14 -0700

    rv - resolving merge conflicts

--- src/main/java/edu/ucsb/cs56/ucsb_courses_search/service/CurriculumService.java
+++ src/main/java/edu/ucsb/cs56/ucsb_courses_search/service/CurriculumService.java
@@ -7,9 +7,6 @@ public interface CurriculumService {
     public String getGE(String college, String area,String quarter);
     public String getGE(String college, String area,String quarter, int startT);
     public String getCourse(String course, int quarter);
-<<<<<<< HEAD
     public String getCSV(String subjectArea, String quarter, String courseLevel, String dept, String instructor, String course, String college, String areas);
-=======
     public String getFinalExam(String quarter, String enrollCode);
->>>>>>> ba8cc2d9bf3d7c0cd9f46e366d3f8b371e12e3a5
 }

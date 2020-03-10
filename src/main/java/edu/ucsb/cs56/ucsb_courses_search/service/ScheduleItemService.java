fd1b0b8b2ea5cc34af3e613acbdcc3e4067308f4
Zonkman <aaa633@umail.ucsb.edu>
2020-03-09 20:02:28 -0700

    new service for ScheduleItem entity functions
    mainly the "deleteByClassname"

---
+++ src/main/java/edu/ucsb/cs56/ucsb_courses_search/service/ScheduleItemService.java
@@ -0,0 +1,29 @@
+package edu.ucsb.cs56.ucsb_courses_search.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import edu.ucsb.cs56.ucsb_courses_search.entity.ScheduleItem;
+import edu.ucsb.cs56.ucsb_courses_search.repository.ScheduleItemRepository;
+
+import java.util.List;
+
+@Service
+public class ScheduleItemService {
+
+    @Autowired
+    private ScheduleItemRepository scheduleItemRepository;
+
+    public ScheduleItemService(){
+    }
+
+    public List<ScheduleItem> deleteByClassname(List<ScheduleItem> courseList, String name){
+        for (ScheduleItem i : courseList){
+            if (i.getClassname().equals(name)){
+                scheduleItemRepository.delete(i);
+                courseList.remove(i);
+            }
+        }
+        return courseList;
+    }
+}
